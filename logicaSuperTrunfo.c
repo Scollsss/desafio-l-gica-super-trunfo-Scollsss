f1b9bdf816bbfa9b91a9804499de57004d218c37
Eduardo Amorim <duamorimalves2@gmail.com>
2025-09-08 00:32:04 +0000

    adicionado printf

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -8,7 +8,7 @@
 int main() {
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
-
+printf("hello,world\n");
     
     // Cadastro das Cartas:
     // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
