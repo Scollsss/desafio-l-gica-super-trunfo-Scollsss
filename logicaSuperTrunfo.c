23e8249248e1fe7522f21bb3bdfad9a7cd0cf361
Eduardo Amorim <duamorimalves2@gmail.com>
2025-09-08 01:29:14 +0000

    Variáveis e scan'fs

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -8,7 +8,14 @@
 int main() {
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
-printf("hello,world\n");
+    
+    char estado[3], estado2[3];
+    char codigo[10], codigo2[10];
+    char cidade[40], cidade2[40];
+    int populacao, populacao2;
+    float area, area2;
+    float pib, pib2;
+    int pontosturisticos, pontosturisticos2;
     
     // Cadastro das Cartas:
     // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
@@ -20,6 +27,28 @@ printf("hello,world\n");
     // scanf("%s", codigo);
     // 
     // (Repita para cada propriedade)
+
+    printf("Digite o estado da primeira carta: ");
+    scanf("%2s", estado);
+
+    printf("Digite o codigo da primeira carta: ");
+    scanf("%9s", codigo);
+
+    printf("Digite o nome da cidade da primeira carta: ");
+    scanf(" %[^\n]", cidade);  // permite espacos
+
+    printf("Digite a populacao da primeira carta: ");
+    scanf("%d", &populacao);
+
+    printf("Digite a area da primeira carta: ");
+    scanf("%f", &area);
+
+    printf("Digite o PIB da primeira carta: ");
+    scanf("%f", &pib);
+
+    printf("Digite o numero de pontos turisticos da primeira carta: ");
+    scanf("%d", &pontosturisticos);
+
 
     // Comparação de Cartas:
     // Desenvolva a lógica de comparação entre duas cartas.
